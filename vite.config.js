7131041fdb661ec7bb18731478329ec962de325a
salin-hub <vongsalin0854.com>
2025-01-21 13:12:14 +0700

    Fix react-bootstrap Carousel import issue

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,19 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
 
+// https://vite.dev/config/
 export default defineConfig({
-    plugins: [react()],
-    build: {
-        rollupOptions: {
-            external: ['react-bootstrap', 'bootstrap'],
-        },
-    },
-});
+  plugins: [react()],
+})
+
+// import { defineConfig } from 'vite';
+// import react from '@vitejs/plugin-react';
+
+// export default defineConfig({
+//     plugins: [react()],
+//     build: {
+//         rollupOptions: {
+//             external: ['react-bootstrap', 'bootstrap'],
+//         },
+//     },
+// });
